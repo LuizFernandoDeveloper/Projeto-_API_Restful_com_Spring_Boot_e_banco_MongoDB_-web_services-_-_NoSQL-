5533664a502cae305c5c1b289ee927b06471adeb
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-11-29 13:32:27 -0300

    Atulizacao de usuario com PUT

--- src/main/java/com/luiz/mongodb/services/UserService.java
+++ src/main/java/com/luiz/mongodb/services/UserService.java
@@ -2,8 +2,10 @@ package com.luiz.mongodb.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.luiz.mongodb.domain.User;
@@ -37,6 +39,17 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
+	public User update(User obj) {
+		User newObjt = findById(obj.getId());
+		updateData(newObjt , obj);
+		return repository.save(newObjt);
+	}
+	
+	private void updateData(User newObjt, User obj) {
+		newObjt.setName(obj.getName());
+		newObjt.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
